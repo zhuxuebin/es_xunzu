944ba29b91b8f4571abf66631246fe2a738120c5
zhuxuebin <zhuxuebin2014@163.com>
2018-05-08 22:36:42 +0800

    first commit

---
+++ es-xunzu-project/src/main/java/com/zxb/xunzu/EsXunzuProjectApplication.java
@@ -0,0 +1,12 @@
+package com.zxb.xunzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsXunzuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsXunzuProjectApplication.class, args);
+	}
+}
