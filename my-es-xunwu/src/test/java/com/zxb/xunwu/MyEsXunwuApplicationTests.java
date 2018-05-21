c320d4388ce7d82c95e44524c7c8e42d26dd9487
xuery <zhuxuebin@sf-express.com>
2018-05-21 12:43:03 +0800

    my-es-xunwu/.gitignore

---
+++ my-es-xunwu/src/test/java/com/zxb/xunwu/MyEsXunwuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxb.xunwu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyEsXunwuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
